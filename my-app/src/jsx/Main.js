8a81e576035e3124080c9fbceeb925cf07ab213d
AdamJednacz <adamjednacz4@gmail.com>
2024-03-24 23:52:15 +0100

    animated btn

--- my-app/src/jsx/Main.js
+++ my-app/src/jsx/Main.js
@@ -10,12 +10,11 @@ const Main = () => {
 
     return (
         <section style={stlye} className="main">
-
             <div className="container">
+
                 <h1>Car Sharing</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, minus.</p>
                 <div className="main_btns">
-                    <button className="main_btn">Share a car</button>
                     <button className="main_btn">See more</button>
                 </div>
             </div>
