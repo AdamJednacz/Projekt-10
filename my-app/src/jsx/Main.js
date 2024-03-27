cc718350781525a575fb797ed44cce67a260b1f0
Laptop <adamjednacz4@gmail.com>
2024-03-27 12:10:15 +0100

    butonny,seervice.scss

--- my-app/src/jsx/Main.js
+++ my-app/src/jsx/Main.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import mainbg from "../assets/Main.webp"
-import Header from "./Header";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircleArrowDown } from '@fortawesome/free-solid-svg-icons'
 import arrow from "../assets/arrow down.webp"
 const Main = () => {
 
@@ -11,7 +7,6 @@ const Main = () => {
     return (
         <section className="main">
             <div className="container">
-
                 <h1>Car Sharing</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, minus.</p>
                 <div className="main_btns">
--- my-app/src/jsx/Service.js
+++ my-app/src/jsx/Service.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 const Service = () => {
     return (
-        <div>
-            
-        </div>
+        <section className="service">
+            <div className="container">
+            <h1>Service</h1>
+                <div className="service_items">
+                    <div className="service_item">
+                        <h2></h2>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
